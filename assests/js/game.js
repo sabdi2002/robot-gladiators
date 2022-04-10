a166084f3c1344b34251e8300db623dfb67c26a2
Salma <salmaabdi7494@gmail.com>
2022-04-10 01:47:01 -0500

    fixes fight game

--- assests/js/game.js
+++ assests/js/game.js
@@ -14,7 +14,7 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-  while(enemyHealth > 0){
+  while(enemyHealth > 0 && playerHealth>0){
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -26,6 +26,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
+      break;
     } else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
@@ -39,6 +40,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
+      break;
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
@@ -52,7 +54,8 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   if (confirmSkip) {
     window.alert(playerName + " has decided to skip this fight. Goodbye!");
     // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
+    playerMoney = playerMoney - 10;
+    console.log("playerMoney", playerMoney);
   }
   // if no (false), ask question again by running fight() again
   else {
